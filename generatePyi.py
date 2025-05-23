9b44175efc6d7a2ddd5d738c526449597510db0f
lyj <1334738575@qq.com>
2025-05-23 17:42:47 +0800

    update

--- generatePyi.py
+++ generatePyi.py
@@ -18,7 +18,11 @@ def generate_stubs(pyd_file, output_dir=None):
 
 
 if __name__ == "__main__":
+    #path is relative to workspace of vscode, current open directory path,
+    # workspace:D:/testPybind11, example_name:D:/testPybind11/build/example/Release/example_Py.pyd
     example_name = 'build.example.Release.example_Py'
+    #this path is append to example_name's directory
+    # output_dir_name:D:/testPybind11/build/example/Release/./
     output_dir_name = '.'
     generate_stubs(example_name, output_dir_name)
 
